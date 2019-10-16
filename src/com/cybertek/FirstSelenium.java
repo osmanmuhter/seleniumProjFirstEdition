0e9f5fa375d23ab0b5c242da72932f61fddd3a99
osmanmuhter <osman.muhter@gmail.com>
2019-10-15 18:29:23 -0700

    second commit

--- src/com/cybertek/FirstSelenium.java
+++ src/com/cybertek/FirstSelenium.java
@@ -1,16 +1,17 @@
 package com.cybertek;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstSelenium {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.gecko.driver",
-                "/Users/osman/Documents/selenium dependencies /drivers/geckodriver");
-        WebDriver driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver",
+                "/Users/osman/Documents/selenium dependencies /drivers/chromedriver");
+        WebDriver driver;
+        driver = new Chro();
 
-        driver.get("https://google.com");
+        driver.get("https://youtube.com");
 
     }
 }
